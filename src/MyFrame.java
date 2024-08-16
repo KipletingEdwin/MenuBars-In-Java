dc9017364316fc9019e059a2bb7c0c6217cf4179
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-16 08:43:48 +0300

    add a new instance of menuBar

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,8 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        this.setVisible(true);
+    }
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
